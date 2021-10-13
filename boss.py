0ef1a97cc2c65b1a8fd5fb5be0fcd4f435dfa7dc
Unique Dev <91629125+dev-unik@users.noreply.github.com>
2021-10-13 23:34:40 +0530

    Update boss.py

--- boss.py
+++ boss.py
@@ -1,1 +1,11 @@
 //Code here
+
+# initializing list
+test_list = ['1', '4', '3', '6', '7']
+
+print ("Original list is : " + str(test_list))
+
+for i in range(0, len(test_list)):
+	test_list[i] = int(test_list[i])
+	
+print ("Modified list is : " + str(test_list))
